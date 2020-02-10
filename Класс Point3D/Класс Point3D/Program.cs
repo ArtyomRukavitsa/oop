11cdd03e7d9d52c11005a088366b85532be1ccbb
Artyom <rukavitsa.a@gmail.com>
2020-02-10 19:15:17 +0300

    Практика 13 (часть 2)

--- Класс Point3D/Класс Point3D/Program.cs
+++ Класс Point3D/Класс Point3D/Program.cs
@@ -8,9 +8,9 @@ namespace Класс_Point3D
 {
     class Point3D
     {
-        public int x;
-        public int y;
-        public int z;
+        private int x;
+        private int y;
+        private int z;
 
         public Point3D()
         {
@@ -18,21 +18,24 @@ namespace Класс_Point3D
             y = 0;
             z = 0;
         }
+
         public Point3D(int x_coord, int y_coord, int z_coord)
         {
-            x = x_coord;
-            y = y_coord;
-            z = z_coord;
+            this.x = x_coord;
+            this.y = y_coord;
+            this.z = z_coord;
         }
+
         public void Move(char direction, int value)
         {
-            if (direction == 'x') x += value;
-            else if (direction == 'y') y += value;
-            else z += value;
+            if (direction == 'x') this.x += value;
+            else if (direction == 'y') this.y += value;
+            else this.z += value;
         }
+
         public void MyPrint()
         {
-            Console.WriteLine("Координаты точки: ({0}, {1}, {2})", x, y, z);
+            Console.WriteLine("Координаты точки: ({0}, {1}, {2})", this.x, this.y, this.z);
         }
 
     }
@@ -40,11 +43,11 @@ namespace Класс_Point3D
     {
         static void Main(string[] args)
         {
-            Point3D point = new Point3D();
-            Console.Write("Если хотите использовать конструктор по умолчанию, введите 1, иначе 2: ");
+            Point3D point;
+            Console.Write("Если хотите использовать конструктор по умолчанию, введите 1, иначе любую цифру: ");
             int command = int.Parse(Console.ReadLine());
             if (command == 1) point = new Point3D();
-            else if (command == 2)
+            else
             {
                 Console.Write("Введите координату x: ");
                 int x = int.Parse(Console.ReadLine());
@@ -54,14 +57,12 @@ namespace Класс_Point3D
                 int z = int.Parse(Console.ReadLine());
                 point = new Point3D(x, y, z);
             }
-            else { Console.WriteLine("Я Вас не понимаю... Завершаю работу"); Environment.Exit(0);}
 
-            
             char direction; // Направление для сдвига точки
             int value; // На сколько сдвигать точку
             while (true)
             {
-                Console.Write("Введите 1 для сдвига точки, введите 2 для вывода координат точки, введите 3 для выхода: ");
+                Console.Write("Введите 1 для сдвига точки, введите 2 для вывода координат точки, иначе введите любую цифру для выхода: ");
                 command = int.Parse(Console.ReadLine());
                 if (command == 1)
                 {
